cbe99886cd76b624e447a917faad09685684cc1d
DmitryChebykin <78965652+DmitryChebykin@users.noreply.github.com>
2022-02-14 00:30:32 +0700

    add mapper scan for mybatis repositories

--- src/main/java/com/example/admitad/AdmitadApplication.java
+++ src/main/java/com/example/admitad/AdmitadApplication.java
@@ -1,11 +1,11 @@
 package com.example.admitad;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
+@MapperScan(basePackages = "com.example.admitad.repository")
 public class AdmitadApplication {
 
     public static void main(String[] args) {
