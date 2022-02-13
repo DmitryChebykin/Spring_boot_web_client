a4bf5f7a48bc814a222e12a9ae2909b4b716a744
DmitryChebykin <78965652+DmitryChebykin@users.noreply.github.com>
2022-02-14 00:32:40 +0700

    add mybatis repository

---
+++ src/main/java/com/example/admitad/repository/ProgramRepository.java
@@ -0,0 +1,15 @@
+package com.example.admitad.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
+
+@Mapper
+public interface ProgramRepository {
+    @Update("CREATE TABLE  IF NOT EXISTS program (id INTEGER PRIMARY KEY AUTO_INCREMENT, " +
+            "name VARCHAR(255) NOT NULL," +
+            "image_uri VARCHAR(255)," +
+            "image BLOB," +
+            "products_xml_link VARCHAR(255)," +
+            "gotoLink VARCHAR(255))")
+    void createTableIfMissing();
+}
